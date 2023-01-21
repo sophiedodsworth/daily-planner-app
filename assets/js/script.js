83e5a1e146f408de1fdacf763f00a859d72b8cfc
Sophie <sophiedodsworth@hotmail.com>
2023-01-21 21:38:16 +0000

    taking values from jquery and saving in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,26 @@
 var currentDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(currentDate);
 
-    // collect data from the local storage
+//  code included within this will only run once the page DOM is ready for the js code to execute
+
+$(document).ready(function () {
+
+// calls function when the save button is clicked
+
+    $(".saveBtn").on("click", function () {
+
+// relates to jquery
+
+        var text = $(this).siblings(".entry").val();
+        var time = $(this).parent().attr("id");
+
+// saves the text inputted by the user in local storage
+
+        localStorage.setItem(time, text);
+
+    })
+
+// collect data from the local storage
 
     $("#hour8 .entry").val(localStorage.getItem("hour8"));
     $("#hour9 .entry").val(localStorage.getItem("hour9"));
@@ -15,3 +34,6 @@ $("#currentDay").html(currentDate);
     $("#hour15 .entry").val(localStorage.getItem("hour15"));
     $("#hour16 .entry").val(localStorage.getItem("hour16"));
     $("#hour17 .entry").val(localStorage.getItem("hour17"));
+
+    timeTracker();
+})
