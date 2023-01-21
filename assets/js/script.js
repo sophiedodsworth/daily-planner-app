d9fc7318854890caf0b9c0ac82ad2f0d2f93fbc4
Sophie <sophiedodsworth@hotmail.com>
2023-01-21 20:02:13 +0000

    adding local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,14 @@
 
 var currentDate = moment().format('dddd, MMM Do YYYY');
 $("#today").html(currentDate);
+
+// collect data from the local storage
+$("#9AM .description").val(localStorage.getItem("9AM"));
+$("#10AM .description").val(localStorage.getItem("10AM"));
+$("#11AM .description").val(localStorage.getItem("11AM"));
+$("#12PM .description").val(localStorage.getItem("12AM"));
+$("#1PM .description").val(localStorage.getItem("1PM"));
+$("#2PM .description").val(localStorage.getItem("2PM"));
+$("#3PM .description").val(localStorage.getItem("3PM"));
+$("#4PM .description").val(localStorage.getItem("4PM"));
+$("#5PM .description").val(localStorage.getItem("5PM"));
