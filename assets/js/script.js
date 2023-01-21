787a1d0703e0d8e3b00e4401fa841a3d9db535e1
Sophie <sophiedodsworth@hotmail.com>
2023-01-21 21:25:25 +0000

    fixed code to make css work properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,17 @@
 // display the current date and time
 
 var currentDate = moment().format('dddd, MMM Do YYYY');
-$("#today").html(currentDate);
+$("#currentDay").html(currentDate);
 
-// collect data from the local storage
-$("#9AM .description").val(localStorage.getItem("9AM"));
-$("#10AM .description").val(localStorage.getItem("10AM"));
-$("#11AM .description").val(localStorage.getItem("11AM"));
-$("#12PM .description").val(localStorage.getItem("12AM"));
-$("#1PM .description").val(localStorage.getItem("1PM"));
-$("#2PM .description").val(localStorage.getItem("2PM"));
-$("#3PM .description").val(localStorage.getItem("3PM"));
-$("#4PM .description").val(localStorage.getItem("4PM"));
-$("#5PM .description").val(localStorage.getItem("5PM"));
+    // collect data from the local storage
+
+    $("#hour8 .entry").val(localStorage.getItem("hour8"));
+    $("#hour9 .entry").val(localStorage.getItem("hour9"));
+    $("#hour10 .entry").val(localStorage.getItem("hour10"));
+    $("#hour11 .entry").val(localStorage.getItem("hour11"));
+    $("#hour12 .entry").val(localStorage.getItem("hour12"));
+    $("#hour13 .entry").val(localStorage.getItem("hour13"));
+    $("#hour14 .entry").val(localStorage.getItem("hour14"));
+    $("#hour15 .entry").val(localStorage.getItem("hour15"));
+    $("#hour16 .entry").val(localStorage.getItem("hour16"));
+    $("#hour17 .entry").val(localStorage.getItem("hour17"));
