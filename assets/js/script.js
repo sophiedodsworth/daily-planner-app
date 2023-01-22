51bb75b776c12433c318bee45994fc73a56f018c
Sophie <sophiedodsworth@hotmail.com>
2023-01-22 11:20:01 +0000

    completed js file with function and if statement to make the daily planner app responsive to the time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // display the current date and time
 
-var currentDate = moment().format('dddd, MMM Do YYYY');
+var currentDate = moment().format('dddd [the] Do [of] MMMM');
 $("#currentDay").html(currentDate);
 
 //  code included within this will only run once the page DOM is ready for the js code to execute
@@ -22,6 +22,49 @@ $(document).ready(function () {
 
     })
 
+// declaring function
+
+function workDayTracker() {
+
+// declaring current time variable with the current hour
+
+    var currentTime = moment().hour();
+
+// loop to go over the timeblocks
+
+    $(".time-block").each(function () {
+        var blockOfTime = parseInt($(this).attr("id").split("hour")[1]);
+
+// start of if else statement
+// if the block of time is less than the current time, this method removes the future and present class names from the selected elements and adds the past class
+
+        if (blockOfTime < currentTime) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+
+// continuation of the if else statement
+// if the block of time is the same as the current time, this method removes the past and future class names from the selected elements and adds the present class
+
+        else if (blockOfTime === currentTime) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+
+// end of the if else statement
+// if the block of time is anything else other than what's been execudted in the if else statement so far, this method removes the past and present class names from the selected elements and adds the future class
+
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+
+        }
+    })
+}
+
 // collect data from the local storage
 
     $("#hour8 .entry").val(localStorage.getItem("hour8"));
@@ -35,5 +78,8 @@ $(document).ready(function () {
     $("#hour16 .entry").val(localStorage.getItem("hour16"));
     $("#hour17 .entry").val(localStorage.getItem("hour17"));
 
-    timeTracker();
+
+// running the function
+
+    workDayTracker();
 })
